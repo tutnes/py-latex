16dad71a9abdf7a039a95fb5c2eb8eee5f4e9adb
root <tutnes@gmail.com>
2017-01-14 09:52:45 +0000

    First commit

---
+++ test.py
@@ -0,0 +1,12 @@
+min_latex = (r"\documentclass{article}"
+             r"\begin{document}"
+             r"Hello, world!"
+             r"\end{document}")
+
+from latex import build_pdf
+
+# this builds a pdf-file inside a temporary directory
+pdf = build_pdf(min_latex)
+
+# look at the first few bytes of the header
+# print bytes(pdf)[:10]
